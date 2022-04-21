6f595e0a4a6d4d15c19bb330a459f7c0cf1066d0
Kevin <kayaswp@gmail.com>
2022-04-21 21:29:24 +0630

    watch the current user

--- src/views/ChatroomView.vue
+++ src/views/ChatroomView.vue
@@ -5,9 +5,24 @@
 </template>
 
 <script>
+import { watch } from '@vue/runtime-core'
 import NavbarView from '../components/NavbarView'
+import getUser from '../compasables/getUser'
+import {useRouter} from 'vue-router'
+
 export default {
   components: { NavbarView },
+
+  setup(){
+    let {user} = getUser();
+    let router = useRouter();
+
+    watch(user,()=>{      //first parameter(can be watch anything), second parameter change base upon first one
+      if(!user.value){    // if there is no user send to another route
+        router.push({name:"Welcome"})   // ('/')
+      }
+    })
+  }
 
 }
 </script>
